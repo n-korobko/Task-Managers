65dda23888410716d26a49717c36bdcb03c791fc
Nikita <hkorobko@gmail.com>
2026-02-03 01:35:32 +0200

    Fix mentor feedback: remove pycache, CBV registration, CRUD UI

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,15 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from .forms import WorkerRegistrationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.shortcuts import redirect
 
-def register(request):
-    if request.user.is_authenticated:
-        return redirect("core:task-list")
 
-    if request.method == 'POST':
-        form = WorkerRegistrationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = "accounts/register.html"
+    success_url = reverse_lazy("login")
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
             return redirect("core:task-list")
-    else:
-        form = WorkerRegistrationForm()
-    return render(request, 'accounts/register.html', {'form': form})
+        return super().dispatch(request, *args, **kwargs)
